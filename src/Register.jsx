97d86686b245b89d80c575c8185ce9e803395e00
Kanasjnr <141998286+Kanasjnr@users.noreply.github.com>
2023-11-10 10:09:25 +0100

    a

--- src/Register.jsx
+++ src/Register.jsx
@@ -6,3 +6,7 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from "./api/axios";
+
+
+const USER_REGEX = /^[A-Z][A-Z0-9-_]{3,23}$/
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/
