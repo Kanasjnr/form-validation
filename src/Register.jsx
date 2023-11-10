16d78445967bc6b0ec2d5de07f96efe06169515e
Kanasjnr <141998286+Kanasjnr@users.noreply.github.com>
2023-11-10 10:03:02 +0100

    a

---
+++ src/Register.jsx
@@ -0,0 +1,8 @@
+import { useRef, useEffect, useState } from "react";
+import {
+  faCheck,
+  faTimes,
+  faInfoCircles,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import axios from "./api/axios";
