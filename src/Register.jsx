125cbdfd04bc89f456e235744eb02f6a6c8500f3
Kanasjnr <141998286+Kanasjnr@users.noreply.github.com>
2023-11-10 10:34:13 +0100

    a

--- src/Register.jsx
+++ src/Register.jsx
@@ -7,8 +7,8 @@ import {
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from "./api/axios";
 
-const USER_REGEX = /^[A-Z][A-Z0-9-_]{3,23}$/;
-const PWD_REGEX = /^(?=.*[a-z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+const USER_REGEX = /^[A-Z][a-z0-9-_]{3,23}$/;
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
 const REGISTER_URL = "./Register.jsx";
 
 const Register = () => {
